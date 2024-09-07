4643d97684312dfbda2e6ac68e8204cca6638a1a
maximetourzel <max.tourzel@outlook.fr>
2024-09-07 23:46:13 +0200

    feat(typeorm): Add typeorm config and packages

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { BankMovementsModule } from './bank-movements/bank-movements.module';
 import { BalanceCheckpointsModule } from './balance-checkpoints/balance-checkpoints.module';
 import { BankSyncValidationsModule } from './bank-sync-validations/bank-sync-validations.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        typeOrmConfig(configService),
+    }),
     BankMovementsModule,
     BalanceCheckpointsModule,
     BankSyncValidationsModule,
