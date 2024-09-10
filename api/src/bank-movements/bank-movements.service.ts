46ec0571b5cace1c429439c9b9fa7d8a28349150
maximetourzel <max.tourzel@outlook.fr>
2024-09-10 20:05:26 +0200

    feat(movements): Add JSdoc for service

--- api/src/bank-movements/bank-movements.service.ts
+++ api/src/bank-movements/bank-movements.service.ts
@@ -12,6 +12,14 @@ export class BankMovementsService {
     private readonly bankMovementsRepository: Repository<BankMovement>,
     private readonly periodsService: PeriodsService,
   ) {}
+
+  /**
+   * Creates a new bank movement in the given period
+   * @param periodId the id of the period
+   * @param createBankMovementDto the data of the new bank movement
+   * @returns the created bank movement
+   * @throws {NotFoundException} if the period is not found
+   */
   async create(
     periodId: string,
     createBankMovementDto: CreateBankMovementDto,
@@ -26,18 +34,34 @@ export class BankMovementsService {
     return this.bankMovementsRepository.save(newBankMovement);
   }
 
+  /**
+   * Finds all bank movements for the given period
+   * @param periodId the id of the period
+   * @returns a promise of an array of bank movements
+   */
   findAllForPeriod(periodId: string): Promise<BankMovement[]> {
     return this.bankMovementsRepository.find({
       where: { period: { id: periodId } },
     });
   }
 
+  /**
+   * Finds all bank movements for the given period
+   * @param periodId the id of the period
+   * @returns a promise of an array of bank movements
+   */
   findByPeriodId(periodId: string): Promise<BankMovement[]> {
     return this.bankMovementsRepository.find({
       where: { period: { id: periodId } },
     });
   }
 
+  /**
+   * Finds a bank movement by its id
+   * @param id the id of the bank movement to find
+   * @returns a promise of the bank movement
+   * @throws {NotFoundException} if the bank movement with the given id is not found
+   */
   findOneById(id: string): Promise<BankMovement> {
     const bankMovement = this.bankMovementsRepository.findOneBy({ id });
     if (!bankMovement) {
@@ -46,6 +70,12 @@ export class BankMovementsService {
     return bankMovement;
   }
 
+  /**
+   * Removes a bank movement
+   * @param id the id of the bank movement to remove
+   * @returns the removed bank movement
+   * @throws {NotFoundException} if the bank movement with the given id is not found
+   */
   async remove(id: string): Promise<BankMovement> {
     const bmToRemove = await this.findOneById(id);
     return this.bankMovementsRepository.remove(bmToRemove);
