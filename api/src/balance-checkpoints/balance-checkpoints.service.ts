6cc3c6414d8a03712c0b4ac12d83ee32fe9b861b
maximetourzel <max.tourzel@outlook.fr>
2024-09-10 20:04:01 +0200

    feat(checkpoints): Add JSdoc for service

--- api/src/balance-checkpoints/balance-checkpoints.service.ts
+++ api/src/balance-checkpoints/balance-checkpoints.service.ts
@@ -13,6 +13,14 @@ export class BalanceCheckpointsService {
     private readonly balanceCheckpointsRepository: Repository<BalanceCheckpoint>,
     private readonly periodsService: PeriodsService,
   ) {}
+
+  /**
+   * Creates a new balance checkpoint for a given period.
+   *
+   * @param periodId The id of the period for the balance checkpoint.
+   * @param createBalanceCheckpointDto The data for the new balance checkpoint.
+   * @returns The newly created balance checkpoint.
+   */
   async create(
     periodId: string,
     createBalanceCheckpointDto: CreateBalanceCheckpointDto,
@@ -27,12 +35,25 @@ export class BalanceCheckpointsService {
     return this.balanceCheckpointsRepository.save(newBalanceCheckpoint);
   }
 
+  /**
+   * Finds all balance checkpoints for a given period.
+   *
+   * @param periodId The id of the period to find balance checkpoints for.
+   * @returns An array of balance checkpoints for the given period.
+   */
   findByPeriodId(periodId: string): Promise<BalanceCheckpoint[]> {
     return this.balanceCheckpointsRepository.find({
       where: { period: { id: periodId } },
     });
   }
 
+  /**
+   * Finds a balance checkpoint by id.
+   *
+   * @param id The id of the balance checkpoint to find.
+   * @returns The balance checkpoint with the given id.
+   * @throws {NotFoundException} If no balance checkpoint with the given id exists.
+   */
   findOneById(id: string): Promise<BalanceCheckpoint> {
     const balanceCheckpoint = this.balanceCheckpointsRepository.findOneBy({
       id,
@@ -43,6 +64,14 @@ export class BalanceCheckpointsService {
     return balanceCheckpoint;
   }
 
+  /**
+   * Updates a balance checkpoint.
+   *
+   * @param id The id of the balance checkpoint to update.
+   * @param updateBalanceCheckpointDto The data to update the balance checkpoint with.
+   * @returns The updated balance checkpoint.
+   * @throws {NotFoundException} If no balance checkpoint with the given id exists.
+   */
   async update(
     id: string,
     updateBalanceCheckpointDto: UpdateBalanceCheckpointDto,
@@ -52,6 +81,13 @@ export class BalanceCheckpointsService {
     return this.balanceCheckpointsRepository.save(balanceCheckpointToUpdate);
   }
 
+  /**
+   * Removes a balance checkpoint.
+   *
+   * @param id The id of the balance checkpoint to remove.
+   * @returns The removed balance checkpoint.
+   * @throws {NotFoundException} If no balance checkpoint with the given id exists.
+   */
   async remove(id: string): Promise<BalanceCheckpoint> {
     const balanceCheckpointToRemove = await this.findOneById(id);
     return this.balanceCheckpointsRepository.remove(balanceCheckpointToRemove);
