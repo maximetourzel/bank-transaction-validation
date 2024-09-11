0837a582bd84890030cc909c71b902fb927ffeca
maximetourzel <max.tourzel@outlook.fr>
2024-09-11 02:36:37 +0200

    test(movements): Add unit tests for service and controller

--- api/src/bank-movements/bank-movements.controller.spec.ts
+++ api/src/bank-movements/bank-movements.controller.spec.ts
@@ -1,20 +1,103 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BankMovementsController } from './bank-movements.controller';
 import { BankMovementsService } from './bank-movements.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('BankMovementsController', () => {
   let controller: BankMovementsController;
+  let service: BankMovementsService;
+
+  const mockBankMovementsService = {
+    create: jest.fn(),
+    findAllForPeriod: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BankMovementsController],
-      providers: [BankMovementsService],
+      providers: [
+        BankMovementsService,
+        {
+          provide: BankMovementsService,
+          useValue: mockBankMovementsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<BankMovementsController>(BankMovementsController);
+    service = module.get<BankMovementsService>(BankMovementsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new bank movement', async () => {
+      const createBankMovementDto = {
+        date: new Date(),
+        wording: 'Salary',
+        amount: 3000,
+      };
+      const periodId = 'uuid';
+      const savedBankMovement = {
+        id: 'uuid',
+        ...createBankMovementDto,
+      };
+      mockBankMovementsService.create.mockResolvedValue(savedBankMovement);
+      const result = await controller.create(periodId, createBankMovementDto);
+      expect(result).toEqual(savedBankMovement);
+      expect(service.create).toHaveBeenCalledWith(
+        periodId,
+        createBankMovementDto,
+      );
+      expect(service.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findAllForPeriod', () => {
+    it('should return an array of bank movements', async () => {
+      const periodId = 'uuid';
+      const bankMovements = [
+        {
+          id: 'uuid',
+          date: new Date(),
+          wording: 'Salary',
+          amount: 3000,
+        },
+      ];
+      mockBankMovementsService.findAllForPeriod.mockResolvedValue(
+        bankMovements,
+      );
+      const result = await controller.findAllForPeriod(periodId);
+      expect(result).toEqual(bankMovements);
+      expect(service.findAllForPeriod).toHaveBeenCalledWith(periodId);
+      expect(service.findAllForPeriod).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a bank movement', async () => {
+      const bankMovement = {
+        id: 'uuid',
+        date: new Date(),
+        wording: 'Salary',
+        amount: 3000,
+      };
+      mockBankMovementsService.remove.mockResolvedValue(bankMovement);
+      await controller.remove(bankMovement.id);
+      expect(service.remove).toHaveBeenCalledWith(bankMovement.id);
+      expect(service.remove).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a NotFoundException if bank movement is not found', async () => {
+      const id = 'uuid';
+      mockBankMovementsService.remove.mockRejectedValueOnce(
+        new NotFoundException(),
+      );
+      await expect(controller.remove(id)).rejects.toThrow(NotFoundException);
+      expect(service.remove).toHaveBeenCalledWith(id);
+    });
   });
 });
--- api/src/bank-movements/bank-movements.service.spec.ts
+++ api/src/bank-movements/bank-movements.service.spec.ts
@@ -1,18 +1,171 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BankMovementsService } from './bank-movements.service';
+import { BankMovement } from './entities/bank-movement.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PeriodsService } from '../periods/periods.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('BankMovementsService', () => {
-  let service: BankMovementsService;
+  let movementService: BankMovementsService;
+
+  const mockBankMovementsRepository = {
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const mockPeriodsService = {
+    findOneById: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BankMovementsService],
+      providers: [
+        BankMovementsService,
+        {
+          provide: getRepositoryToken(BankMovement),
+          useValue: mockBankMovementsRepository,
+        },
+        {
+          provide: PeriodsService,
+          useValue: mockPeriodsService,
+        },
+      ],
     }).compile();
 
-    service = module.get<BankMovementsService>(BankMovementsService);
+    movementService = module.get<BankMovementsService>(BankMovementsService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(movementService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new bank movement', async () => {
+      const periodId = 'uuid';
+      const createBankMovementDto = {
+        date: new Date(),
+        wording: 'Salary',
+        amount: 3000,
+      };
+      const period = {
+        id: 'uuid',
+      };
+      const savedBankMovement = {
+        id: 'uuid',
+        ...createBankMovementDto,
+      };
+      mockPeriodsService.findOneById.mockResolvedValueOnce(period);
+      mockBankMovementsRepository.create.mockReturnValueOnce(savedBankMovement);
+      mockBankMovementsRepository.save.mockResolvedValueOnce(savedBankMovement);
+      const result = await movementService.create(
+        periodId,
+        createBankMovementDto,
+      );
+      expect(result).toEqual(savedBankMovement);
+      expect(mockPeriodsService.findOneById).toHaveBeenCalledTimes(1);
+      expect(mockPeriodsService.findOneById).toHaveBeenCalledWith(periodId);
+      expect(mockBankMovementsRepository.create).toHaveBeenCalledTimes(1);
+      expect(mockBankMovementsRepository.create).toHaveBeenCalledWith({
+        ...createBankMovementDto,
+        period,
+      });
+      expect(mockBankMovementsRepository.save).toHaveBeenCalledTimes(1);
+      expect(mockBankMovementsRepository.save).toHaveBeenCalledWith(
+        savedBankMovement,
+      );
+    });
+  });
+
+  describe('findAllForPeriod', () => {
+    it('should return an array of bank movements', async () => {
+      const periodId = 'uuid';
+      const bankMovements = [
+        {
+          id: 'uuid',
+        },
+      ];
+      mockBankMovementsRepository.find.mockResolvedValueOnce(bankMovements);
+      const result = await movementService.findAllForPeriod(periodId);
+      expect(result).toEqual(bankMovements);
+      expect(mockBankMovementsRepository.find).toHaveBeenCalledTimes(1);
+      expect(mockBankMovementsRepository.find).toHaveBeenCalledWith({
+        where: { period: { id: periodId } },
+      });
+    });
+  });
+
+  describe('findByPeriodId', () => {
+    it('should return an array of bank movements', async () => {
+      const periodId = 'uuid';
+      const bankMovements = [
+        {
+          id: 'uuid',
+        },
+      ];
+      mockBankMovementsRepository.find.mockResolvedValueOnce(bankMovements);
+      const result = await movementService.findByPeriodId(periodId);
+      expect(result).toEqual(bankMovements);
+      expect(mockBankMovementsRepository.find).toHaveBeenCalledWith({
+        where: { period: { id: periodId } },
+      });
+    });
+  });
+
+  describe('findOneById', () => {
+    it('should return a bank movement', async () => {
+      const id = 'uuid';
+      const bankMovement = {
+        id: 'uuid',
+      };
+      mockBankMovementsRepository.findOneBy.mockResolvedValueOnce(bankMovement);
+      const result = await movementService.findOneById(id);
+      expect(result).toEqual(bankMovement);
+      expect(mockBankMovementsRepository.findOneBy).toHaveBeenCalledWith({
+        id,
+      });
+    });
+
+    it('should throw a NotFoundException if bank movement is not found', async () => {
+      const id = 'uuid';
+      mockBankMovementsRepository.findOneBy.mockResolvedValueOnce(null);
+      await expect(movementService.findOneById(id)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(mockBankMovementsRepository.findOneBy).toHaveBeenCalledWith({
+        id,
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a bank movement', async () => {
+      const id = 'uuid';
+      const bankMovement = {
+        id: 'uuid',
+      };
+      mockBankMovementsRepository.findOneBy.mockResolvedValueOnce(bankMovement);
+      mockBankMovementsRepository.remove.mockResolvedValueOnce(bankMovement);
+      const result = await movementService.remove(id);
+      expect(result).toEqual(bankMovement);
+      expect(mockBankMovementsRepository.remove).toHaveBeenCalledWith(
+        bankMovement,
+      );
+    });
+
+    it('should throw a NotFoundException if bank movement is not found', async () => {
+      const bankMovement = {
+        id: 'uuid',
+      };
+      mockBankMovementsRepository.remove.mockResolvedValueOnce(null);
+      await expect(movementService.remove(bankMovement.id)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(mockBankMovementsRepository.remove).toHaveBeenCalledWith(
+        bankMovement,
+      );
+    });
   });
 });
